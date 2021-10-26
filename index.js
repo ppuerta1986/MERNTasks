8c0ec4c07b85ec03e6c77f8814554590cb6e688a
Pedro Puerta <ppuerta@tupaca.com>
2021-10-26 01:58:51 -0300

    Ajuste para deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 // Puerto del app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -25,6 +25,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
